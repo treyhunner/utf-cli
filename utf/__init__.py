6f807a783f8fe70726237516404a3a78c1446a87
Trey Hunner <trey@treyhunner.com>
2024-07-11 14:44:51 -0700

    Bump version

--- utf/__init__.py
+++ utf/__init__.py
@@ -16,7 +16,7 @@ from textual.widgets import Button, Footer, Header, Input, Static
 from .generate_db import make_database, db_path
 
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 if not db_path.exists():
     make_database()
